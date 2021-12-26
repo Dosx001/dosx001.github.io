4a85c661cc3f6758ae5357bfd56ba54a7d9b7e3d
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-12-26 12:53:18 -0800

    Created Navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <>
+      <div id='nav-background'></div>
+      <nav>
+        <a href='/' id='main'>Andres Rodriguez Michel</a>
+        <a href="/Projects">Projects</a>
+        <a href="/Education">Education</a>
+        <a href="/Workflow">Workflow</a>
+        <a href="/Interests">Interests</a>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
