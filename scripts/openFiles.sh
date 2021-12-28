de09cf5016ef0962b115a76a1f0f128c27da2f55
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-12-28 10:55:09 -0800

    Convert jsx to tsx

--- scripts/openFiles.sh
+++ scripts/openFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd src
 vim -p components/SideBar.scss components/Navbar.scss App.scss \
-    "+vs components/SideBar.jsx | tabn" \
-    "+vs components/Navbar.jsx | tabn" \
-    "+vs App.jsx | tabm 0" \
+    "+vs components/SideBar.tsx | tabn" \
+    "+vs components/Navbar.tsx | tabn" \
+    "+vs App.tsx | tabm 0" \
