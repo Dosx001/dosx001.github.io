ce39be8aed831564ca94c915511031020ce402c5
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-12-28 09:27:37 -0800

    Convert css to scss

--- scripts/openFiles.sh
+++ scripts/openFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd src
-vim -p components/SideBar.css components/Navbar.css App.css \
+vim -p components/SideBar.scss components/Navbar.scss App.scss \
     "+vs components/SideBar.jsx | tabn" \
     "+vs components/Navbar.jsx | tabn" \
     "+vs App.jsx | tabm 0" \
