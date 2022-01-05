1442bb887b34fa8d6f8d744fe34268bb0eadb605
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2022-01-05 12:41:25 -0800

    Added buttons() to ProjectSelector

--- scripts/openFiles.sh
+++ scripts/openFiles.sh
@@ -5,10 +5,11 @@ if [ $# == 0 ]; then
         pages/Workflow.tsx \
         "+vs App.tsx"
 elif [ $1 == 'c' ]; then
-    vim -p components/PostList.scss components/Project.tsx components/ProjectSelector.tsx \
+    vim -p components/PostList.scss components/Project.tsx components/ProjectSelector.scss \
         components/GitHubFeed.scss components/ImageSlider.scss components/SideBar.scss \
         components/Navbar.scss \
-        "+vs components/PostList.tsx | tabn | tabn | tabn" \
+        "+vs components/PostList.tsx | tabn | tabn" \
+        "+vs components/ProjectSelector.tsx | tabn" \
         "+vs components/GitHubFeed.tsx | tabn" \
         "+vs components/ImageSlider.tsx | tabn" \
         "+vs components/SideBar.tsx | tabn" \
--- src/components/ProjectSelector.tsx
+++ src/components/ProjectSelector.tsx
@@ -1,3 +1,4 @@
+import './ProjectSelector.scss';
 import { Dispatch, SetStateAction } from "react";
 
 interface ProjSel {
@@ -15,6 +16,19 @@ const ProjectSelector = ({current, setCurrent, length}:ProjSel) => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
+  const chProj = (e:React.MouseEvent<HTMLButtonElement>) => {
+    setCurrent(parseInt((e.target as HTMLButtonElement).innerText) - 1)
+  }
+
+  const buttons = () => {
+    let btns = [];
+    for (let i = 0; i < length; i++)
+      btns.push(
+        <button onClick={chProj} className={current === i ? 'proj' : ''} key={i}>{i + 1}</button>
+      )
+    return btns
+  }
+
   return (
     <div className="buttons">
       <button onClick={prevProj}>
@@ -22,6 +36,7 @@ const ProjectSelector = ({current, setCurrent, length}:ProjSel) => {
           <path d="M22 8l-13 7l13 7"/>
         </svg>
       </button>
+      { buttons() }
       <button onClick={nextProj}>
         <svg viewBox="0 0 30 30">
           <path d="M8 8l13 7l-13 7"/>
