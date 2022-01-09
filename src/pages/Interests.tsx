561f6abf0065e0a282abd58b292ea2ccd31fd322
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2022-01-09 12:15:25 -0800

    Updated Interests page

--- src/pages/Interests.tsx
+++ src/pages/Interests.tsx
@@ -1,8 +1,55 @@
-import Construction from 'components/Construction';
+import PostList from 'components/PostList';
 
 const Interests = () => {
+  const posts = [
+    {
+      title: "Programming",
+      content: [
+        {
+          tag: "p",
+          inner: "words..."
+        }
+    ]
+    },
+    {
+      title: "Vim",
+      content: [
+        {
+          tag: "p",
+          inner: "words..."
+        }
+    ]
+    },
+    {
+      title: "Linux",
+      content: [
+        {
+          tag: "p",
+          inner: "words..."
+        }
+    ]
+    },
+    {
+      title: "Anime",
+      content: [
+        {
+          tag: "p",
+          inner: "words..."
+        }
+    ]
+    },
+    {
+      title: "Gaming",
+      content: [
+        {
+          tag: "p",
+          inner: "words..."
+        }
+    ]
+    }
+  ]
   return (
-    <Construction/>
+    <PostList posts={posts}/>
   );
 }
 
